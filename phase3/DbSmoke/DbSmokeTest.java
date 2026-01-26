fd7edffa385571bbd7c9a14e10f0ea1c73e37f67
yamane689 <mr.ryohei980@gmail.com>
2026-01-26 21:40:22 +0900

    Add Dao

--- phase3/DbSmoke/DbSmokeTest.java
+++ phase3/DbSmoke/DbSmokeTest.java
@@ -1,28 +1,26 @@
 package phase3.DbSmoke;
 
-import java.sql.*;
+import phase3.dao.TaskDao;
+import phase3.model.Task;
+
+import java.util.List;
 
 public class DbSmokeTest {
     public static void main(String[] args) throws Exception {
         String url =
             "jdbc:mysql://localhost:3306/appdb" +
             "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo";
-
         String user = "root";
         String pass = "rootpass";
 
-        try (Connection con = DriverManager.getConnection(url, user, pass);
-             PreparedStatement ps = con.prepareStatement(
-                 "SELECT id, title, created_at FROM tasks ORDER BY id"
-             );
-             ResultSet rs = ps.executeQuery()) {
+        TaskDao dao = new TaskDao(url, user, pass);
 
-            while (rs.next()) {
-                long id = rs.getLong("id");
-                String title = rs.getString("title");
-                Timestamp createdAt = rs.getTimestamp("created_at");
-                System.out.println(id + " " + title + " " + createdAt);
-            }
+        long newId = dao.insert("dao inserted task");
+        System.out.println("inserted id =" + newId);
+
+        List<Task> tasks = dao.findAll();
+        for(Task t : tasks){
+            System.out.println(t.id + " " + t.title + " " + t.createdAt);
         }
     }
 }
